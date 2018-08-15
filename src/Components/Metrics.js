0aee029a81d5ead85c63a962c76e2b6d68200ebc
Your Name <antiadp@me.com>
2018-08-15 16:09:39 -0600

    DA pull new changes

--- src/Components/Metrics.js
+++ src/Components/Metrics.js
@@ -14,6 +14,7 @@ class Metrics extends Component {
                 <div className="WPM">
                     <h1>{this.props.WPM}</h1>
                     <h4>WPM</h4>
+                    {/* WPM: calc  */}
                 </div>
                 <div className="CPM">
                     <h1>{this.props.CPM}</h1>
