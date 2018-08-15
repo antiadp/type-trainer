66c21151bff83b0ae4beb895da102b818d712b2e
Amy Knudson <amylknudson@gmail.com>
2018-08-15 11:00:32 -0600

    server is set up

---
+++ Server/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+// const ctrl = require('./server-controllers');
+// const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const {
+ SERVER_PORT,
+ CONNECTION_STRING,
+ SESSION_SECRET,
+} = process.env
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('Database reporting for duty');
+});
+
+
+
+
+
+app.listen(SERVER_PORT, () => console.log(`Listening in on ${SERVER_PORT}`));
