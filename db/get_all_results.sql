ebaf9c6940369980a6591dd1d92a527de15349b2
Amy Knudson <amylknudson@gmail.com>
2018-08-16 12:34:24 -0600

    ak refactored SideNav to be a stateful component

--- db/get_all_results.sql
+++ db/get_all_results.sql
@@ -1,3 +1,9 @@
+-- select u.username, u.user_id, u.img, t.test_id, t.wpm, t.cpm, t.accuracy, t.efficiency, t.timestamp
+-- from test_results t, users u
+-- where t.user_id = u.user_id;
+
 select u.username, u.user_id, u.img, t.test_id, t.wpm, t.cpm, t.accuracy, t.efficiency, t.timestamp
 from test_results t, users u
-where t.user_id = u.user_id;
+where t.user_id = u.user_id
+order by t.accuracy desc
+limit 10;
