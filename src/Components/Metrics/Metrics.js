00181c576e366375e80b02a1bd1d7c280de0c05d
unknown <jtray@outlook.es>
2018-08-20 15:42:48 -0600

    jtr timer moved

--- src/Components/Metrics/Metrics.js
+++ src/Components/Metrics/Metrics.js
@@ -7,74 +7,64 @@ class Metrics extends Component {
             keyPresses: 100,
             numErrors: 5,
             iOfLastCharTyped: 0,
-            // totalChars: [].length - iOfLastCharTyped,
-            currentTime: 60,
+            currentTime: 3,
             WPM: 0,
             CPM: 0,
-            accuracy: 0,
-            DEM: 0
-            
+            ACC: 100,
+            DEM: 0,
+            baseTimer:3,
+            snippet:'',
+            userInput:''
         }
     }
-    componentDidUpdate(){
-        // this.setState({
-        //     keyPresses: this.state.keyPresses,
-        //     numErrors: this.state.keyPresses,
-        //     totalChars: this.state.totalChars,
-        //     currentTime: this.state.currentTime,
-        //     DEM: ((this.state.accuracy/100) * (this.state.WPM - this.state.numErrors))
-        // })
+    componentDidMount(){
+ 
     }
+
+ 
+    everySecond = () => {
+        this.setState({
+            currentTime: this.state.currentTime - 1
+        })
+        this.startTimer()
+    }
+    startTimer = () => {
+        if (this.state.currentTime > 0) {
+            setTimeout(this.everySecond, 1000)
+        }else if(this.state.currentTime === 0 || this.state.currentTime<0){
+            return
+    }
+}
     
-    
-    
-    
+
     render() {
-        console.log(this.props.timer)
-        
+
+        if(this.props.userInput.length===1){
+            this.startTimer()
+        }
 
         return (
-            // <div className="metrics-wrapper">
             <div className="metrics-wrapper">
                 <div className="WPM">
-                    <h1>{this.props.WPM}</h1>
-                    {
-                        function WPM(){
-                            let grossWPM = (this.state.keyPresses / 5) / (60)
-                            let netWPM = grossWPM - (this.state.numErrors / 60)
-                                return Math.round(netWPM)
-                        }
-                    }
+                    <h1>{this.state.WPM}</h1>
+                   
                     <h4>WPM</h4>
                 </div>
                 <div className="CPM">
-                    <h1>{this.props.CPM}</h1>
-                    {
-                        function CPM(){
-                            let netCPM = this.state.keyPresses - this.state.numErrors
-                                return netCPM
-                        }
-                    }
+                    <h1>{this.state.CPM}</h1>
+                    
                     <h4>Characters per minute</h4>
                 </div>
                 <div className="ACC">
-                    <h1>{this.props.ACC}</h1>
-                        {
-                            function accuracy(){
-                                let acc = (this.state.keyPresses - this.state.numErrors) / this.state.keyPresses
-                                    return acc
-                            }
-                        }
+                    <h1>{this.state.ACC}%</h1>
+                     
                     <h4>Accuracy</h4>
                 </div>
                 <div className="timer-wrapper">
-                    <h1>{this.props.timer}</h1>
+                    <h1>{this.state.currentTime}</h1>
                     <h4>Seconds</h4>
                 </div>
             </div>
-
-
-            // </div>
         )
     }
 }
