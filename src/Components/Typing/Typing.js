b6583352baa90ad0832334d6b586700033a6fd84
unknown <jtray@outlook.es>
2018-08-21 21:17:41 -0600

    jtr cleaning stuff

--- src/Components/Typing/Typing.js
+++ src/Components/Typing/Typing.js
@@ -15,14 +15,16 @@ class Typing extends Component {
     }
     // ----------------------------------------------------------------------------------
 
+
+    // When component mounts, will make a request for a snippet, will split the array, and turn each element from ascii to readable
+    //Will then set state with that array.
     componentDidMount() {
         axios.get('/api/get-snippet').then(res => {
             let snippet = res.data[0].snippet;
+            let lettersArray = [];
             let snippetArray = snippet.split(',').map((current) => {
                 return Number(current)
             })
-            let lettersArray = [];
-
 
             for (let i = 0; i < snippetArray.length; i++) {
                 lettersArray.push(String.fromCharCode(snippetArray[i]))
@@ -34,12 +36,15 @@ class Typing extends Component {
         })
     }
     // ----------------------------------------------------------------------------------
-
+    // will fire every time a key is pressed. setting state with the input value.
+    // will fire ONLY if 'timerBool' is true;
+    // also sets the input to an ascii array.
     updateUserInput = (value) => {
         if (!this.state.timerBool) {
             this.setState({
                 input: value
             })
+
             let tempArray = []
             for (let i = 0; i < value.length; i++) {
                 tempArray.push(value.charCodeAt(i))
@@ -49,10 +54,12 @@ class Typing extends Component {
             this.setState({
                 asciiArray: asciiArray,
             })
+            // we may have to pass ascii array instead of the letters array.
+            // we may want to pass asciiArray compare the input ascii against the ascii on the backend
         }
     }
     // ----------------------------------------------------------------------------------
-
+    // when fired function will clear the input and the ascii Array on state
     clearMe = () => {
         this.setState({
             input: '',
@@ -60,7 +67,8 @@ class Typing extends Component {
         })
     }
     // ----------------------------------------------------------------------------------
-
+    // Toggles the read only property on the textArea box.  passed through props to 'Metrics' and fired on 'Metrics.js'
+    // fired by 'Metrics' when the timer reaches 0
     toggleReadOnly = () => {
         console.log('fired')
         this.setState({
