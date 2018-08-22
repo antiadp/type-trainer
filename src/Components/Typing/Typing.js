e0c074acd31de6f94d48fb7034fac7a025231eb8
unknown <jtray@outlook.es>
2018-08-21 20:45:07 -0600

    jtr logic2 cleaned

--- src/Components/Typing/Typing.js
+++ src/Components/Typing/Typing.js
@@ -7,15 +7,13 @@ class Typing extends Component {
     constructor(props) {
         super()
         this.state = {
-
             input: '',
-            asciiArray: [65],
-            lettersArray: ['A'],
-            timerBool:false,
+            asciiArray: [32],
+            lettersArray: [' '],
+            timerBool: false,
         }
     }
-
-
+    // ----------------------------------------------------------------------------------
 
     componentDidMount() {
         axios.get('/api/get-snippet').then(res => {
@@ -23,81 +21,90 @@ class Typing extends Component {
             let snippetArray = snippet.split(',').map((current) => {
                 return Number(current)
             })
-
             let lettersArray = [];
+
 
             for (let i = 0; i < snippetArray.length; i++) {
                 lettersArray.push(String.fromCharCode(snippetArray[i]))
             }
-
 
             this.setState({
                 lettersArray: lettersArray
             })
         })
     }
-
-
+    // ----------------------------------------------------------------------------------
 
     updateUserInput = (value) => {
-        if(!this.state.timerBool){
+        if (!this.state.timerBool) {
             this.setState({
-            input: value
-        })
-        let tempArray = []
-        for (let i = 0; i < value.length; i++) {
-            tempArray.push(value.charCodeAt(i))
+                input: value
+            })
+            let tempArray = []
+            for (let i = 0; i < value.length; i++) {
+                tempArray.push(value.charCodeAt(i))
+            }
+            let asciiArray = tempArray.toString()
+
+            this.setState({
+                asciiArray: asciiArray,
+            })
         }
-        
-        let asciiArray = tempArray.toString()
-        this.setState({
-            asciiArray: asciiArray,
-        })
     }
-    }
+    // ----------------------------------------------------------------------------------
 
     clearMe = () => {
-        console.log('No pasting allowed')
         this.setState({
             input: '',
             asciiArray: ''
         })
     }
-    toggleReadOnly = () =>{
+    // ----------------------------------------------------------------------------------
+
+    toggleReadOnly = () => {
         console.log('fired')
         this.setState({
-            timerBool:true
+            timerBool: true
         })
     }
+    // ----------------------------------------------------------------------------------
 
     render() {
         let joined = this.state.lettersArray.join('')
         let input = this.state.input
-        // console.log('ACC',this.state.ACC)
         return (
 
             <div className='typing-wrapper'>
-
                 <Metrics
                     userInput={input}
                     snippet={joined}
                     toggleReadOnly={this.toggleReadOnly}
-
                 />
 
-                <textarea value={this.state.input} onChange={(e) => { this.updateUserInput(e.target.value) }} data-gramm_editor="false" autoComplete='off' spellCheck='false' name="Main Typing input" id="text-input" cols="30" rows="10" placeholder={this.state.placeholder} maxLength='500' readOnly={this.state.timerBool} onCopy={this.clearInput} onDrag={this.clearInput} onDrop={this.clearInput} onPaste={this.clearInput} />
-           
+                <textarea
+                    value={this.state.input}
+                    onChange={(e) => { this.updateUserInput(e.target.value) }}
+                    data-gramm_editor="false"
+                    autoComplete='off'
+                    spellCheck='false'
+                    name="Main Typing input"
+                    id="text-input" cols="30"
+                    rows="10"
+                    placeholder={this.state.placeholder}
+                    maxLength='500' readOnly={this.state.timerBool}
+                    onCopy={this.clearInput}
+                    onDrag={this.clearInput}
+                    onDrop={this.clearInput}
+                    onPaste={this.clearInput}
+                />
+
                 <div className="display-wrapper">
                     <div className="DisplayText">
-                        <p id= 'snippetDisplay'>{this.state.lettersArray}</p>
-                            
+                        <p id='snippetDisplay'>{this.state.lettersArray}</p>
                     </div>
                 </div>
                 <br />
                 <br />
-
-
-
                 <div className="charts">
                     <Charts />
                 </div>
