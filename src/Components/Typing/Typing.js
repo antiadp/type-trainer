5c8b5361deb119c67c8ff37bc26f478a5a3a03f9
unknown <jtray@outlook.es>
2018-08-23 15:51:29 -0600

    jtr-Colorfied and chart

--- src/Components/Typing/Typing.js
+++ src/Components/Typing/Typing.js
@@ -6,12 +6,16 @@ import axios from 'axios';
 class Typing extends Component {
 	constructor(props) {
 		super();
+		this.WPMArray=[]
+		this.ACCArray=[]
+		this.DEM=0
 		this.state = {
 			input: '',
-			asciiArray: [ 32 ],
-			lettersArray: [ ' ' ],
-            timerBool: false,
-            snippetAscii: []
+			asciiArray: [32],
+			lettersArray: [' '],
+			timerBool: false,
+			snippetAscii: [],
+		
 		};
 	}
 
@@ -22,7 +26,7 @@ class Typing extends Component {
 				return Number(current);
 			});
 
-            this.setState({snippetAscii: snippetArray})
+			this.setState({ snippetAscii: snippetArray })
 			let lettersArray = [];
 
 			for (let i = 0; i < snippetArray.length; i++) {
@@ -31,10 +35,10 @@ class Typing extends Component {
 				// }
 				lettersArray.push(String.fromCharCode(snippetArray[i]));
 			}
-            // letterArray is an array of character strings from the snippet script
+			// letterArray is an array of character strings from the snippet script
 			this.setState({
 				lettersArray: lettersArray
-            });
+			});
 		});
 	}
 	// updateUserInput is converting user input to ascii chars and pushing them into asciiArray
@@ -47,9 +51,9 @@ class Typing extends Component {
 			for (let i = 0; i < value.length; i++) {
 				tempArray.push(value.charCodeAt(i));
 			}
-            //  asciiArray is an array of ascii nums
+			//  asciiArray is an array of ascii nums
 			this.setState({
-                asciiArray: tempArray
+				asciiArray: tempArray
 			});
 		}
 	};
@@ -62,11 +66,31 @@ class Typing extends Component {
 		});
 	};
 	toggleReadOnly = () => {
-		console.log('fired');
+		// console.log('Read Only fired');
 		this.setState({
 			timerBool: true
 		});
 	};
+	passChartMetrics = (wpm, acc, dem) => {
+		console.log('chartMetrics Fired')
+		
+		let WPM10Percent = Math.floor(wpm.length / 10)
+		let WPMTemp = [wpm[WPM10Percent], wpm[(WPM10Percent * 2)], wpm[(WPM10Percent * 3)], wpm[(WPM10Percent * 4)], wpm[(WPM10Percent * 5)], wpm[(WPM10Percent * 6)], wpm[(WPM10Percent * 7)], wpm[(WPM10Percent * 8)], wpm[(WPM10Percent * 9)], wpm[wpm.length-1]]
+		let WPMPassed = WPMTemp.map(e=>{return Math.round(e)})
+
+		let ACC10Percent = Math.floor(acc.length /10)
+		let ACCTemp = [acc[ACC10Percent], acc[(ACC10Percent*2)], acc[(ACC10Percent*3)], acc[(ACC10Percent*4)], acc[(ACC10Percent*5)], acc[(ACC10Percent*6)], acc[(ACC10Percent*7)], acc[(ACC10Percent*8)], acc[(ACC10Percent*9)], acc[acc.length-1]]
+		let ACCPassed = ACCTemp.map(e=>{return Math.round(e*100)})
+		// debugger
+		this.WPMArray = WPMPassed
+		this.ACCArray = ACCPassed
+		this.DEM = dem
+		// this.setState({
+		// 	WPMArray:WPMPassed,
+		// 	ACCArray:ACCPassed,
+		// 	DEM:dem
+		// })
+	}
 
 	render() {
 		let classes;
@@ -74,16 +98,17 @@ class Typing extends Component {
 			return (
 				<span key={i} className={classes}>{letter}</span>
 			);
-    });
-        // joined is the snippet script string 
-        // let joined = this.state.lettersArray.join('');
-        
+		});
+		// joined is the snippet script string 
+		// let joined = this.state.lettersArray.join('');
+
 		return (
 			<div className="typing-wrapper">
 				<Metrics
-					userInputAscii = {this.state.asciiArray}
-					snippetAscii = {this.state.snippetAscii}
-					toggleReadOnly = {this.toggleReadOnly}
+					userInputAscii={this.state.asciiArray}
+					snippetAscii={this.state.snippetAscii}
+					toggleReadOnly={this.toggleReadOnly}
+					passChartMetrics={this.passChartMetrics}
 				/>
 
 				<textarea
@@ -113,11 +138,16 @@ class Typing extends Component {
 					</div>
 				</div>
 				<br />
-				<br />
+				{this.props.language}
 
+				{(this.state.snippetAscii.length === this.state.asciiArray.length)?
 				<div className="charts">
-					<Charts />
+					<Charts WPMArray = {this.WPMArray} ACCArray = {this.ACCArray} DEM = {this.DEM} />
 				</div>
+				:<div></div>
+				
+				}
+				
 			</div>
 		);
 	}
