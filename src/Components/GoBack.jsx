d9194209297466d451fa5155b1b079ec23b9697f
unknown <jtray@outlook.es>
2018-10-22 15:28:36 -0600

    linking

---
+++ src/Components/GoBack.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GoBack (){
+    return(
+        <div className="GoBack">
+            <a href="https://jtylerray.com">
+                <button>Go Back To portfolio</button>
+            </a>
+        </div>
+    )
+}
+export default GoBack
